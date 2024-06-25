09f5331a8224ef1b4817f63aa037703d37498417
Reynold97 <reynoldalejandro46@gmail.com>
2024-06-25 20:40:36 +0000

    1st version

--- README.md
+++ README.md
@@ -1,28 +1,77 @@
-### Requirements (Windows)
+### Enviroment 
 
-Create env with conda 
+1-Install miniconda on linux
+
+```bash
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+```
+
+2-After installation init miniconda
+
+```bash
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+```
+
+3-Restart the terminal
+
+4-Clone the repo 
+
+```bash
+git clone https://github.com/Reynold97/YOLOv10-crowd-recog.git
+```
+
+5-Go to folder
+
+```bash
+cd YOLOv10-crowd-recog
+```
+
+6-Create env with conda 
 
 ```bash
 conda create -p ./env python=3.10 -y
 ```
 
-Activate enviroment
+7-Activate enviroment
 
 ```bash
 conda activate ./env
 ```
 
-Install YOLOv10
+8-Install YOLOv10
 
 ```bash
 pip install -q git+https://github.com/THU-MIG/yolov10.git
 ```
 
-Other complementary packages 
+9-Other complementary packages 
 -> Roboflow for training datasets datasets 
 -> Supervision for visualization 
 
 ```bash
-pip install -q supervision roboflow
+pip install -q supervision roboflow 
 ```
 
+10-Download models
+
+```bash
+mkdir -p weights
+```
+
+```bash
+wget https://github.com/THU-MIG/yolov10/releases/download/v1.1/yolov10n.pt -P weights
+```
+
+To work with the results find more details in ultralytics docs
+
+https://docs.ultralytics.com/modes/predict/#working-with-results
+
+https://docs.ultralytics.com/reference/engine/results/
+
+https://docs.ultralytics.com/modes/predict/#boxes
+
+https://docs.ultralytics.com/reference/engine/results/#ultralytics.engine.results.Boxes
--- app.py
+++ app.py
@@ -1,1 +1,49 @@
 from ultralytics import YOLOv10
+import cv2
+import supervision as sv
+
+# Load the model
+model = YOLOv10('weights/yolov10n.pt')
+
+# Read the input image
+image = cv2.imread('data/example_1.jpg')
+
+# Perform object detection
+results = model([image])
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    #result.show()  # display to screen
+    result.save(filename="data/annotated_example_1.jpg")  # save to disk
+
+for r in boxes:
+    print(r.cls)   # print the class  0 is the label for persons and 56 is the label for chairs
+    print(r.conf)  # print the confiability value
+    print(r.xyxy)  # print boxes as xy format
+
+
+#Using supervision 
+'''
+# Convert Ultralytics results to Supervision Detections
+detections = sv.Detections.from_ultralytics(results)
+
+# Initialize annotators
+bounding_box_annotator = sv.BoundingBoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+# Annotate the image with bounding boxes and labels
+annotated_image = bounding_box_annotator.annotate(scene=image, detections=detections)
+annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+
+# Save the annotated image
+output_filename = 'data/annotated_example_1.jpg'
+cv2.imwrite(output_filename, annotated_image)
+
+# Optionally, display the image
+sv.plot_image(annotated_image)
+'''
